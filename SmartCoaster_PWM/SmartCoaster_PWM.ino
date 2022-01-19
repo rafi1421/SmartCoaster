0da3020aab7b53652e280e82926d25aa6dcdefcb
Rafi <rafi1421@gmail.com>
2022-01-18 23:43:02 -0600

    Digispark Demo
    Glow the leds after a few seconds of button press.
    Add pinouts for digispark vs arduino mega

--- SmartCoaster_PWM/SmartCoaster_PWM.ino
+++ SmartCoaster_PWM/SmartCoaster_PWM.ino
@@ -1,9 +1,18 @@
-#define DEBUG_serial true		// serial print messages
+#define DEBUG_serial false		// serial print messages
 
-#define RPIN 2
-#define GPIN 3
+//DigiSpark Pins
+#define RPIN 0
+#define GPIN 1
 #define BPIN 4
-#define BUTTON 22
+#define BUTTON 2
+
+//Arduino Mega Pins
+//#define RPIN 2
+//#define GPIN 3
+//#define BPIN 4
+//#define BUTTON 22
+
+
 #define ADJUSTMENT 55
 
 int counter = 0;
@@ -16,7 +25,7 @@ int adjustmentValue = 0;
 int buttonValue = 0;
 bool buttonDown = false;
 
-int thresh_min=100;
+int thresh_min=50;
 int thresh_mid=150;
 int thresh_max=200;
 
@@ -25,6 +34,7 @@ void setup() {
   #if DEBUG_serial
 	Serial.begin(9600);
   #endif // DEBUG
+
   
   pinMode(RPIN, OUTPUT);
   pinMode(GPIN, OUTPUT);
@@ -37,8 +47,8 @@ void setup() {
 void loop() {
 
   buttonValue = digitalRead(BUTTON);
-  adjustmentValue = analogRead(ADJUSTMENT);
-  int adjustmentRange = map(adjustmentValue, 0, 1024, 1, 25);
+//  adjustmentValue = analogRead(ADJUSTMENT);
+//  int adjustmentRange = map(adjustmentValue, 0, 1024, 1, 25);
 
 #if DEBUG_serial
   Serial.print("ButtonValue: ");
@@ -71,7 +81,7 @@ void loop() {
   evaluateColors();
   updateLights();
   //delay(100* adjustmentRange);
-  delay(1);
+  delay(30);  //Serialprint creates a delay, so adjust delay if not using it. 
 
 }
 
